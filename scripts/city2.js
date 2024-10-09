805024873b317939744f896bda24233e68dcff39
Almaelado <borsny75@gmail.com>
2024-10-09 08:55:39 +0200

    város2.1

---
+++ scripts/city2.js
@@ -0,0 +1,4 @@
+document.getElementById('fact-button').addEventListener('click', function() {
+    const fact = document.getElementById('fact');
+    fact.style.display = fact.style.display === 'none' ? 'block' : 'none';
+});
